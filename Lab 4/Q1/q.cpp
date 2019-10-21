83fe0e79cb3a17e6b7ce5573b41aad221c8abd1f
ankurryder <aj17@iitbbs.ac.in>
2019-10-21 07:34:07 +0530

    Modified Lab 4 Q1

--- Lab 4/Q1/q.cpp
+++ Lab 4/Q1/q.cpp
@@ -17,12 +17,14 @@ int main()  {
     printf("Enter the productions\n");
     for(int i = 0 ;i < n ; i++)
         cin >> s[i];
+    int last = 0;
     for(int k = 0 ; k < n ; k++){
         input = new char [s[k].length()+1];;
         strcpy(input , s[k].c_str());
         l = strtok(input,"->");
         r = strtok(NULL,"->");
         temp = strtok(r,"|");
+        last = i;
         while(temp)  {
             if(temp[0] == l[0])  {
                 flag = 1;
@@ -32,11 +34,11 @@ int main()  {
                 sprintf(productions[i++],"%s->%s%s'\0",l,temp,l);
             temp = strtok(NULL,"|");
         }
-        sprintf(productions[i++],"%s->\u03B5",l);
+        sprintf(productions[i++],"%s'->\u03B5",l);
         if(flag == 0)
             cout <<"\n"<< s[k] << "\n";
         else{
-            for(j=0;j<i;j++)  {
+            for(j=last;j<i;j++)  {
                 printf("\n%s",productions[j]);
             }
         }
